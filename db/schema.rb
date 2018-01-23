eac4ce2ac9262e67e2fc46f16b5265f055c7ce86
akinomaeni <akinomaeni@gmail.com>
2018-01-22 21:30:32 -0800

    Hotel model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123052741) do
+ActiveRecord::Schema.define(version: 20180123052959) do
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
